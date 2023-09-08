d2d3b46731f9bd5cdf1d53da225be3346ff55581
MedinaIvan-95 <medina.ivan.da95@gmail.com>
2023-09-07 21:07:24 -0430

    Proyecto React Footer 4

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -1,7 +1,8 @@
 import "./Footer.css"
-import footerbg from "../../public/Footer.svg"
+
 const Footer = () => {
-    return <footer className='footer' style={{ backgroundImage: `url(${footerbg})` }}>
+    return <footer className='footer'>
+        <img src="/img/Footer.svg"/>
     </footer>
 }
 
